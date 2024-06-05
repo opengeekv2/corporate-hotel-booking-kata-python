324baaa146008c9671389f920d0c32decc16ac75
Marc Mauri Alloza <marcmaurialloza@gmail.com>
2024-06-05 08:33:36 +0200

    implement company missing case in fake repository

--- features/steps/fake_infrastructure/company_repository.py
+++ features/steps/fake_infrastructure/company_repository.py
@@ -1,8 +1,13 @@
+from typing import Optional
+
 from corporate_hotel_booking_kata_python.domain.model import Company
 
 _companies = []
 def save(company: Company):
     _companies.insert(company.id, company)
 
-def get(id: int) -> Company:
-    return _companies[id]
+def get(id: int) -> Optional[Company]:
+    try:
+        return _companies[id]
+    except IndexError:
+        return None
